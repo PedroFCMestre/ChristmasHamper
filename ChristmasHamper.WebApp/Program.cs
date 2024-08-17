43d976f6d511d939438ec23ca4d339e7e8022e9e
PedroFCMestre <38054319+PedroFCMestre@users.noreply.github.com>
2024-08-17 21:59:44 +0000

    Added Blazor UI and ApiClient library projects.

---
+++ ChristmasHamper.WebApp/Program.cs
@@ -0,0 +1,30 @@
+using ChristmasHamper.ApiClient.Services;
+using ChristmasHamper.WebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("http://localhost:5074/"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
